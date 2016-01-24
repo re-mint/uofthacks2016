02cfc97ad4d7cdc244a40cdb3e4ddd081025008c
Howard-F <howard-f@hotmail.com>
2016-01-24 04:42:11 -0500

    reverted changes

--- app/src/main/java/com/uofthacks/HippoApp/StartPage.java
+++ app/src/main/java/com/uofthacks/HippoApp/StartPage.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class Hippo extends AppCompatActivity {
+public class StartPage extends AppCompatActivity {
     private Handler mHandler = new Handler();
 
     @Override
