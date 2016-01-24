cb862bb72773b6f4e52b78d41caec1f68bb53583
re-mint <babak.eric@gmail.com>
2016-01-24 03:19:57 -0500

    confirmation screen

---
+++ app/src/main/java/com/uofthacks/HippoApp/HelpOnway.java
@@ -0,0 +1,93 @@
+package com.uofthacks.HippoApp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.uofthacks.sosapp.R;
+
+public class HelpOnway extends AppCompatActivity implements LocationListener {
+
+    private TextView latitudeField;
+    private TextView longitudeField;
+    private LocationManager locationManager;
+    private String provider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_onway);
+
+        latitudeField = (TextView) findViewById(R.id.lat);
+        longitudeField = (TextView) findViewById(R.id.lon);
+
+        // Get the location manager, and....
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+
+        // Handle security exception, and... initialize the location fields
+        try {
+            Location location = locationManager.getLastKnownLocation(provider);
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location); // ...this also sets the text view
+
+        } catch (SecurityException e) {
+            latitudeField.setText("Location not available");
+            longitudeField.setText("Location not available");
+        }
+
+
+        /*
+        if (location != null) {
+            System.out.println("Provider " + provider + " has been selected.");
+            onLocationChanged(location);
+        } else {
+            latitudeField.setText("Location not available");
+            longitudeField.setText("Location not available");
+        } */
+
+    }
+
+
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(this, "Enabled new provider " + provider,
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        latitudeField.setText(String.valueOf(lat));
+        longitudeField.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Toast.makeText(this, "Disabled provider " + provider,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void packageLocation(String provider) {
+
+    }
+}
