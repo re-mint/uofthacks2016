3f9fc310392eb944e8557100629b14d1f714b627
Howard-F <howard-f@hotmail.com>
2016-01-24 04:20:21 -0500

    signup UI

--- app/src/main/java/com/uofthacks/HippoApp/SignUpActivity.java
+++ app/src/main/java/com/uofthacks/HippoApp/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.uofthacks.HippoApp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,7 +12,13 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
     }
 
+    public void onClickSubmit(View view){
+        Toast toast = Toast.makeText(getApplicationContext(),
+                "Thanks for signing up!", Toast.LENGTH_SHORT);
+        toast.show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
